786f3f4ce069d200b24e25ff80e86a4def848b9d
NaidDzhan Durmush <nc.durmush@gmail.com>
2021-03-27 21:09:57 +0200

    add jquery add furniture browse tab

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function (){
+    $('h3').click(function(){
+        window.location.href='browse.html'
+    })
+
+    $('#logo').click(function (){
+        window.location.href='index.html'
+    })
+})
+
+
