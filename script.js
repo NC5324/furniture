be92614a6a39f35c7b408f267bd808b18fea77cf
NaidDzhan Durmush <nc.durmush@gmail.com>
2021-03-29 00:46:06 +0300

    add footer links update back-to-top button style

--- script.js
+++ script.js
@@ -16,6 +16,19 @@ $(document).ready(function (){
     button.style.display = 'none'
     const jumbotron = document.querySelector('.jumbotron')
 
+    let lastScrollTop = 0
+
+    document.querySelector('.jumbotron').onscroll = function () {
+
+        let st = jumbotron.scrollTop
+        let st2 = document.querySelector('html').scrollTop
+        console.log(st, st2)
+        $("html, body").animate({ scrollTop: st2 + st }, 100);
+
+        lastScrollTop = st <= 0 ? 0 : st;
+        return false;
+    }
+
     $(window).scroll(function () {
         if(window.scrollY > 100) {
             button.style.display = 'block'
@@ -23,6 +36,13 @@ $(document).ready(function (){
         else {
             button.style.display = 'none'
         }
+    })
+
+    $('#fb').click(function(){
+        window.location.href="http://facebook.com"
+    })
+    $('#insta').click(function(){
+        window.location.href="http://instagram.com"
     })
 })
 
